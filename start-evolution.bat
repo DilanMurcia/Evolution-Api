9d5c651e70cd29171ac10b74908c3bdedaa8d9ac
Dilan Albert Murcia Parra <dilanalbertmurcia@gmail.com>
2025-07-27 12:12:25 -0500

    Sincronizar prisma

--- start-evolution.bat
+++ start-evolution.bat
@@ -8,5 +8,8 @@ timeout /t 50
 echo Ejecutando prisma generate...
 docker exec evolution_api npx prisma generate --schema=./prisma/mysql-schema.prisma
 
+echo Ejecutando prisma studio...
+docker exec -it evolution_api npx prisma studio --schema=./prisma/mysql-schema.prisma
+
 echo Listo.
 pause
--- start-evolution.sh
+++ start-evolution.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo Levantando contenedores...
 docker-compose up -d
+
 echo "⏳ Esperando 50 segundos a que inicie evolution_api..."
 sleep 50
+
 echo "▶️ Ejecutando Prisma Generate en el contenedor..."
 docker exec evolution_api npx prisma generate --schema=./prisma/mysql-schema.prisma
 
-echo "✅ Prisma generado. Puedes usar la API."
+echo "▶️ Ejecutando Prisma Studio en el contenedor..."
+docker exec -it evolution_api npx prisma studio --schema=./prisma/mysql-schema.prisma
+
+echo "✅ Prisma generado y studio ejecutado. Puedes usar la API."
